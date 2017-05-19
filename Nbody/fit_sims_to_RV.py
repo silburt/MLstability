b73e5288a8529e940e2b6d58b109f9cc3c3e0ce6
silburt <silburt@astro.utoronto.ca>
2017-05-18 21:35:08 -0400

    Nbody

---
+++ Nbody/fit_sims_to_RV.py
@@ -0,0 +1,45 @@
+#This script migrates planets with small and large masses into MMR so that I can measure the libration amplitude. This will ultimately constrain what the minimum sampling frequency can be for my FFT features for the MLStability project. I want to capture these libration amplitudes as they will be important for measuring any instability.
+
+import multiprocessing as mp
+import os
+import sys
+import random
+import numpy as np
+import pandas as pd
+import rebound
+import glob
+from progress.bar import Bar
+
+def make_runs(N_runs):
+    #draw masses from the posterior
+    m1 = 1e-4
+    m2 = 1e-4
+    #make N_runs for simulation
+    random.seed()
+    runs = []
+    mig_rate = random.sample(np.round(np.logspace(3,6,10*N_runs)), N_runs)
+    K1 = random.sample(np.logspace(-1,3,10*N_runs), N_runs)
+    K2 = random.sample(np.logspace(-1,3,10*N_runs), N_runs)
+    path = 'output/'
+    for i in xrange(0,N_runs):
+        seed = int(10000*random.random())
+        name = path+'taueinner_migrate%.1e_Kin%.1e_Kout%.1e_sd%d'%(mig_rate[i],K1[i],K2[i],seed)
+        runs.append((m1,m2,mig_rate[i],K1[i],K2[i],seed,name))
+    return runs
+
+#each pool worker executes this
+def execute(pars):
+    os.system('./rebound %f %f %f %f %f %d %s'%pars)
+
+#Main multiprocess execution - Give sysname and letters of outer planets close to resonance
+#############Main Code##############################
+if __name__== '__main__':
+    os.system('make')
+    N_runs = 2
+    runs = make_runs(N_runs)
+    
+    pool = mp.Pool(processes=np.min([N_runs, 5]))
+    pool.map(execute, runs)
+    pool.close()
+    pool.join()
+
---
+++ Nbody/orbits.py
@@ -0,0 +1,71 @@
+#This checks whether the planets are still in resonance or not. 
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import matplotlib.cm as cm
+import re
+
+colors=['b','g','m','r','c','y']
+
+file_name=str(sys.argv[1])
+
+fos = open(''+file_name, 'r')
+time, dE, N, mig_rate, damp1, damp2, migtime, DT, a1, e1, a2, e2, phi1, phi2, phi3, m1, m2, taua1, taue1, taua2, taue2 = np.loadtxt(fos, delimiter=',', unpack=True)
+
+ms=6
+fig, axes = plt.subplots(nrows=5, ncols=1, figsize=(10,13), sharex=True)
+plt.subplots_adjust(hspace = 0.2)
+
+P1 = (a1[0]**3 /0.92)**0.5 * 2*np.pi
+time /= P1
+
+#axes[0].set_xlim([1e5, 1.002e5])
+
+#plot
+axes[0].plot(time, phi1, '.', ms=ms, markeredgecolor='none', label='phi 1')
+axes[0].plot(time, phi2, '.', ms=ms, markeredgecolor='none', alpha=0.2, label='phi 2')
+#axes[0].plot(time, phi3, '.', ms=ms, markeredgecolor='none', label='phi 3')
+#axes[0].plot(time, dE,'o',ms=ms, markeredgecolor='none')
+axes[1].plot(time, a1, 'o', ms=ms, markeredgecolor='none')
+axes[1].plot(time, a2, 'o', ms=ms, markeredgecolor='none')
+axes[2].plot(time, (a2/a1)**(1.5), 'o', ms=ms, markeredgecolor='none')
+axes[3].plot(time, e1, 'o', ms=ms, markeredgecolor='none', label='planet 1')
+axes[3].plot(time, e2, 'o', ms=ms, markeredgecolor='none', label='planet 2')
+
+P2 = a2**1.5
+axes[4].plot(time, taue1/P2, 'o', ms=2*ms, color='black', markeredgecolor='none', label='tau_e1')
+axes[4].plot(time, taue2/P2, 'o', ms=ms, color='green', markeredgecolor='none', label='tau_e2')
+axes[4].plot(time, taua2/P2, 'o', ms=ms/3, color='red', markeredgecolor='none', label='tau_a1 ')
+axes[4].set_yscale('log')
+axes[4].legend(loc='upper left', fontsize=10)
+
+plot_bounds = 1
+if plot_bounds == 1:
+    mig_time = migtime[0]/P1
+    D_T = DT[0]/P1
+    axes[0].plot([mig_time, mig_time], [0, 2*np.pi], 'r--',lw=4)
+    axes[0].plot([D_T, D_T], [0, 2*np.pi], 'c--',lw=4)
+    axes[1].plot([mig_time, mig_time], [0, max(a2)], 'r--',lw=4)
+    axes[1].plot([D_T, D_T], [0, max(a2)], 'c--',lw=4)
+    axes[2].plot([mig_time, mig_time], [1, 3], 'r--',lw=4)
+    axes[2].plot([D_T, D_T], [1, 3], 'c--',lw=4)
+    axes[3].plot([mig_time, mig_time], [0, max([max(e1),max(e2)])], 'r--',lw=4)
+    axes[3].plot([D_T, D_T], [0, max([max(e1),max(e2)])], 'c--',lw=4)
+
+#labelling
+#axes[0].set_xscale('log')
+axes[0].legend(loc='upper left')
+#axes[0].set_xlim([100,max(time)])
+#axes[0].set_xlim([18000,19000])
+axes[0].set_ylabel('Resonant Angle', fontsize=13)
+axes[1].set_ylabel('Semi-major axis', fontsize=13)
+axes[2].set_ylabel('Period Ratio', fontsize=13)
+axes[3].set_ylabel('Eccentricity', fontsize=13)
+axes[4].set_xlabel('Orbital Period of Inner Planet', fontsize=13)
+axes[4].set_ylabel('Damping', fontsize=13)
+axes[4].set_yscale('log')
+
+file_output_name = re.sub('\.txt$', '', file_name)
+plt.savefig(file_output_name+'_orbit.png')
+#plt.show()
